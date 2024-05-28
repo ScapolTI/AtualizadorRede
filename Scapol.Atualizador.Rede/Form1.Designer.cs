f86e3bb6fa580a9d8c443c9a6c54d937537cf8e9
FabioCruzScapol <fabio.cruz@scapol.com.br>
2024-05-28 11:49:32 -0300

    Atualização

--- Scapol.Atualizador.Rede/Form1.Designer.cs
+++ Scapol.Atualizador.Rede/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variável de designer necessária.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Limpar os recursos que estão sendo usados.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,122 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código gerado pelo Windows Form Designer
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sttlblMensagem = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sttlblMensagemErro = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblSelArquivo = new System.Windows.Forms.Label();
+            this.edtCaminhoArquivo = new System.Windows.Forms.TextBox();
+            this.btnSelArquivo = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sttlblMensagem,
+            this.sttlblMensagemErro});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sttlblMensagem
+            // 
+            this.sttlblMensagem.Name = "sttlblMensagem";
+            this.sttlblMensagem.Size = new System.Drawing.Size(0, 16);
+            // 
+            // sttlblMensagemErro
+            // 
+            this.sttlblMensagemErro.Name = "sttlblMensagemErro";
+            this.sttlblMensagemErro.Size = new System.Drawing.Size(0, 16);
+            // 
+            // lblSelArquivo
+            // 
+            this.lblSelArquivo.AutoSize = true;
+            this.lblSelArquivo.Location = new System.Drawing.Point(12, 9);
+            this.lblSelArquivo.Name = "lblSelArquivo";
+            this.lblSelArquivo.Size = new System.Drawing.Size(108, 16);
+            this.lblSelArquivo.TabIndex = 1;
+            this.lblSelArquivo.Text = "Caminho arquivo";
+            // 
+            // edtCaminhoArquivo
+            // 
+            this.edtCaminhoArquivo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.edtCaminhoArquivo.Location = new System.Drawing.Point(12, 28);
+            this.edtCaminhoArquivo.Name = "edtCaminhoArquivo";
+            this.edtCaminhoArquivo.ReadOnly = true;
+            this.edtCaminhoArquivo.Size = new System.Drawing.Size(543, 22);
+            this.edtCaminhoArquivo.TabIndex = 2;
+            // 
+            // btnSelArquivo
+            // 
+            this.btnSelArquivo.Location = new System.Drawing.Point(561, 12);
+            this.btnSelArquivo.Name = "btnSelArquivo";
+            this.btnSelArquivo.Size = new System.Drawing.Size(227, 38);
+            this.btnSelArquivo.TabIndex = 3;
+            this.btnSelArquivo.Text = "Selecionar Arquivo";
+            this.btnSelArquivo.UseVisualStyleBackColor = true;
+            this.btnSelArquivo.Click += new System.EventHandler(this.btnSelArquivo_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 87);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(12, 68);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(0, 16);
+            this.lblContador.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSelArquivo);
+            this.Controls.Add(this.edtCaminhoArquivo);
+            this.Controls.Add(this.lblSelArquivo);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Atualizador Rede";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sttlblMensagem;
+        private System.Windows.Forms.ToolStripStatusLabel sttlblMensagemErro;
+        private System.Windows.Forms.Label lblSelArquivo;
+        private System.Windows.Forms.TextBox edtCaminhoArquivo;
+        private System.Windows.Forms.Button btnSelArquivo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblContador;
     }
 }
+
--- Scapol.Atualizador.Rede/Form1.cs
+++ Scapol.Atualizador.Rede/Form1.cs
@@ -1,10 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Scapol.Atualizador.Rede
 {
     public partial class Form1 : Form
     {
+        string Usuario;
+        string cnStr;
+        string cnStrSQL;
+        string tSQL;
+        string UserSAP;
+        string SenhaSAP;
+        long lConexao = 1;
+        int lErrCode;
+        string sErrMsg;
+        object rowsAffected;
+        int totalLinhas = 0;
+        int contador = 1;
+
+        SAPbobsCOM.Company oCompany;
+
+        ADODB.Connection cn;
+        ADODB.Recordset rs;
+        ADODB.Command cmd;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = new ADODB.Connection();
+            rs = new ADODB.Recordset();
+
+
+            Usuario = WindowsIdentity.GetCurrent().Name.Replace(@"SCAPOL\", "");
+            Usuario = WindowsIdentity.GetCurrent().Name.Replace(@"SCAPOL\", "");
+
+            //string de conexão
+            cnStr = "DSN=hanab1; UID=SYSTEM; PWD=h1n1Sc1p4l";
+
+            cn.Open(cnStr);
+
+            tSQL = $@" Select ""U_usersap"",""U_senhasap"" from SBH_SCAPOL.""@TBUSUARIOS"" t where t.""U_userwindows"" = '" + Usuario + "' ";
+
+            cn.CommandTimeout = 360;
+            rs = cn.Execute(tSQL, out rowsAffected);
+
+            if (!rs.EOF)
+            {
+                UserSAP = rs.Fields["u_usersap"].Value.ToString();
+                SenhaSAP = rs.Fields["u_senhasap"].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Usuário Windows não mapeado com usuário SAP - SBH_SCAPOL.@TBUSUARIOS");
+            }
+
+            cn.Close();
+
+            sttlblMensagemErro.Text = "Aguarde alguns segundos enquanto a conexão é efetuada.";
+            Refresh();
+
+            oCompany = new SAPbobsCOM.Company();
+
+            oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_HANADB;
+
+            oCompany.Disconnect();
+            oCompany.Server = "NDB@hanab1:30013";
+            oCompany.UseTrusted = false;
+            oCompany.DbUserName = "SYSTEM";
+            oCompany.DbPassword = "h1n1Sc1p4l";
+            oCompany.CompanyDB = "SBH_SCAPOL";
+            oCompany.UserName = UserSAP;
+            oCompany.Password = SenhaSAP;
+
+            lConexao = oCompany.Connect();
+
+            if (lConexao != 0)
+            {
+                oCompany.GetLastError(out lErrCode, out sErrMsg);
+                MessageBox.Show("Não foi possível estabelecer a conexão!" + Environment.NewLine + "Por favor tentar novamente!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                sttlblMensagemErro.Text = "Erro ao conectar: " + sErrMsg;                
+            }
+            else
+            {
+                sttlblMensagem.Text = "Conectado: " + oCompany.CompanyName;
+                sttlblMensagemErro.Text = string.Empty;
+            }
+        }
+
+        private void btnSelArquivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Arquivos CSV";
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Arquivos CSV|*.CSV";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                edtCaminhoArquivo.Text = openFileDialog.FileName;
+                
+                totalLinhas = File.ReadLines(openFileDialog.FileName).Count();
+
+                lblContador.Text = $"0/{totalLinhas}";
+                //btnProcessar.Enabled = true;
+            }
         }
     }
 }
